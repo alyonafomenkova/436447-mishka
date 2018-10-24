1a58320774287d8efa33ae6058f5564ee63c8d71
Alyona Fomenkova <fruitella30sep@mail.ru>
2018-10-24 17:32:42 +0300

    JS for popup.

---
+++ source/js/popup.js
@@ -0,0 +1,24 @@
+var link = document.querySelectorAll(".js-order");
+var popup = document.querySelector(".modal");
+var overlay = document.querySelectorAll(".modal__overlay");
+
+for (var i = 0; i < link.length; i++) {
+  link[i].addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+  });
+}
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+    }
+  }
+});
+
+overlay.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
