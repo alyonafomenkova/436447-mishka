a86df457f5f76907c9a81b296a2a70245f4be3c1
Alyona Fomenkova <fruitella30sep@mail.ru>
2018-10-31 21:47:27 +0300

    Fixing JS.

--- source/js/popup.js
+++ source/js/popup.js
@@ -3,13 +3,13 @@ var popup = document.querySelector(".modal");
 var overlay = document.querySelector(".modal__overlay");
 
 for (var i = 0; i < link.length; i++) {
-  link[i].addEventListener("click", function(evt) {
+  link[i].addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
   });
 }
 
-window.addEventListener("keydown", function(evt) {
+window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     if (popup.classList.contains("modal-show")) {
@@ -18,7 +18,9 @@ window.addEventListener("keydown", function(evt) {
   }
 });
 
-overlay.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal-show");
-});
+if (overlay) {
+  overlay.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+  });
+}
